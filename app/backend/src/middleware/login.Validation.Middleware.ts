d7d0a3d3c997a81bc7a899756544432c624df111
carolfaro <caroline.faro@gmail.com>
2022-10-06 20:58:09 -0300

    building /login/validate route

--- app/backend/src/middleware/login.Validation.Middleware.ts
+++ app/backend/src/middleware/login.Validation.Middleware.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-// import HttpException from '../shared/http.exception';
+import * as jwt from 'jsonwebtoken';
+import User from 'src/database/models/User';
+import HttpException from '../shared/http.exception';
+import IToken from '../entities/IToken';
+import IUser from '../entities/IUser';
 
 class LoginValidationMiddleware {
   static async verifyEmptyFields(req: Request, res: Response, next: NextFunction) {
@@ -7,6 +11,16 @@ class LoginValidationMiddleware {
 
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
+    }
+
+    next();
+  }
+
+  static async validateEmailPassword(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw new HttpException(400, 'Provide a token');
     }
 
     next();
--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -5,5 +5,6 @@ import LoginValidationMiddleware from '../middleware/login.Validation.Middleware
 const router = Router();
 
 router.post('/login', LoginValidationMiddleware.verifyEmptyFields, AuthController.authLogin);
+router.get('/login/validate', LoginValidationMiddleware.validateEmailPassword);
 
 export default router;
